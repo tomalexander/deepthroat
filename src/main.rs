3f73319a8b464a06758de8bb333b22bb0157f379
Tom Alexander <craftkiller@gmail.com>
2016-09-07 23:29:51 -0700

    assigning next and prev day strings

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,14 @@ extern crate serde_json;
 
 pub mod db;
 pub mod hierarchy;
-
-use std::collections::BTreeMap;
+pub mod output;
 
 fn main() {
     let conn = db::open_db();
     let rooms = db::get_rooms(&conn);
     for room in rooms {
         let messages = db::get_messages_for_room(&conn, &room);
-        let grouped_by_date = group_messages_by_date(messages);
-        for (date, messages_that_day) in &grouped_by_date {
-            println!("{}: {} messages", date, messages_that_day.len());
-        }
-        // println!("{}", hierarchy::get_room_path(&room).to_string_lossy());
+        output::process_messages_for_room(&room, messages);
     }
 }
 
-fn group_messages_by_date(messages: Vec<db::DbMessage>) -> BTreeMap<String, Vec<db::DbMessage>> {
-    let mut ret: BTreeMap<String, Vec<db::DbMessage>> = BTreeMap::new();
-    for msg in messages {
-        let key: String = msg.get_date_string();
-        ret.entry(key).or_insert(Vec::new()).push(msg);
-    }
-    ret
-}
---
+++ src/output.rs
@@ -0,0 +1,44 @@
+use db;
+use std::collections::BTreeMap;
+
+#[derive(Debug)]
+pub struct RoomDay {
+    previous_date: Option<String>,
+    current_date: String,
+    next_date: Option<String>,
+    messages: Vec<db::DbMessage>,
+}
+
+pub fn process_messages_for_room(room: &db::DbRoom, messages: Vec<db::DbMessage>) {
+    let grouped_by_date = group_messages_by_date(messages);
+    let mut room_days: Vec<RoomDay> = Vec::new();
+    let mut previous_date: Option<String> = None;
+    for (date, messages_that_day) in grouped_by_date {
+        match room_days.last_mut() {
+            Some(ref mut prev_room_day) => {
+                prev_room_day.next_date = Some(date.clone());
+            },
+            None => ()
+        };
+        room_days.push(RoomDay {
+            previous_date: previous_date.clone(),
+            current_date: date.clone(),
+            next_date: None,
+            messages: messages_that_day,
+        });
+        previous_date = Some(date);
+    }
+    println!("Room: {}", room.id);
+    for room_day in room_days {
+        println!("{:?}", room_day);
+    }
+}
+
+fn group_messages_by_date(messages: Vec<db::DbMessage>) -> BTreeMap<String, Vec<db::DbMessage>> {
+    let mut ret: BTreeMap<String, Vec<db::DbMessage>> = BTreeMap::new();
+    for msg in messages {
+        let key: String = msg.get_date_string();
+        ret.entry(key).or_insert(Vec::new()).push(msg);
+    }
+    ret
+}
